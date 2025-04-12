22488498886670769aaa565ef58dc3bb0f8cf20e
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2025-04-12 16:37:20 +0530

    add frontend for home page

--- website/src/components/Animation.tsx
+++ website/src/components/Animation.tsx
@@ -5,7 +5,7 @@ import Spline from '@splinetool/react-spline';
 const SplineScene: React.FC = () => {
   return (
     <div>
-      <Spline scene="https://prod.spline.design/BjNbEgw8YrZb0QTs/scene.splinecode?v=5" />
+      <Spline scene="https://prod.spline.design/BjNbEgw8YrZb0QTs/scene.splinecode?v=6" />
     </div>
   );
 };
--- website/src/components/Profile.tsx
+++ website/src/components/Profile.tsx
@@ -241,7 +241,7 @@ export default function ProfilePage() {
   }
 
   return (
-    <div className="bg-gray-900 text-gray-100 rounded-xl p-6 min-h-full relative overflow-hidden">
+    <div className="scroll-container overflow-y-auto pb-8 flex items-center justify-center min-h-screen text-white p-4 overflow-hidden bg-gray-900 text-gray-100 rounded-xl p-6 min-h-full relative overflow-hidden">
       {/* Background decorative elements */}
       <div className="absolute top-0 right-0 w-64 h-64 bg-purple-900 rounded-full filter blur-3xl opacity-20 -translate-y-1/2 translate-x-1/4"></div>
       <div className="absolute bottom-0 left-0 w-96 h-96 bg-blue-900 rounded-full filter blur-3xl opacity-20 translate-y-1/3 -translate-x-1/4"></div>
