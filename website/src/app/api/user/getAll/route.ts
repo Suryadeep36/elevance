8dd3f432f2943ef3d45373323660dbaf7853b60a
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2025-04-19 12:05:10 +0530

    make api route for getAllUser secure

--- website/src/app/api/user/getAll/route.ts
+++ website/src/app/api/user/getAll/route.ts
@@ -4,29 +4,27 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  request: NextRequest,
-) {
+export async function GET(request: NextRequest) {
   try {
     await dbConnect();
-    
+
     const allUsers = await UserModel.find({});
 
     if (!allUsers) {
-      return NextResponse.json(
-        { message: 'User not found' },
-        { status: 404 }
-      );
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
-
-    return NextResponse.json(
-      { success: true, allUsers },
-      { status: 200 }
-    );
+    const filteredUsers: any = [];
+    allUsers.map((ele) => {
+      filteredUsers.push({
+        username: ele.name,
+        metamaskAddress: ele.metamaskAddress,
+      });
+    });
+    return NextResponse.json({ success: true, allUsers: filteredUsers }, { status: 200 });
   } catch (error) {
     console.error("Error fetching user:", error);
     return NextResponse.json(
-      { error: 'Failed to retrieve user' },
+      { error: "Failed to retrieve user" },
       { status: 500 }
     );
   }
